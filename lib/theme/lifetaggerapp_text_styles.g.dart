fbe2a30b99e3d3be2fe589bfabcc92eff3f4255e
Parabeac-Nest <admin@parabeac.com>
2022-10-18 14:30:52 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/lifetaggerapp_text_styles.g.dart
@@ -0,0 +1,220 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class LifetaggerappTextStyles {
+
+static const TextStyle otherTextStylesInfo = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle otherTextStylesInfoBold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle otherTextStylesInfoBigBold = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle otherTextStylesButtonLg = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle otherTextStylesButtonSm = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subtitle2 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Public Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle displayLargeHeavy1200+ = TextStyle(
+  fontSize: 69,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Source Sans Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle f1Heavy1200+ = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Source Sans Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline1 = TextStyle(
+  fontSize: 64,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline2 = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline3 = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline4 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline5 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline6 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subtitle1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subtitle2 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyText1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyText2 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle caption = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle overline = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle button = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Satoshi Variable',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
